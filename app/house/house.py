d7d4c67a660d3e4e55a1648b4fd265bcca20b6f2
flybetter <flybetter@163.com>
2018-10-14 09:23:28 +0800

    some css style modify

--- app/house/house.py
+++ app/house/house.py
@@ -3,10 +3,12 @@ from flask import render_template
 from redis import Redis
 import json
 import pandas as pd
+import traceback
+from . import newhouse
 
 # TODO 调试的用的redis
 r = Redis(host='192.168.10.221', port=6379, db=1)
-r2 = Redis(host='192.168.10.221', port=6379, db=2)
+# r2 = Redis(host='192.168.10.221', port=6379, db=2)
 
 NEWHOUSELOG_PREFIX = "NHLOG^"
 PHONEDEVICE_PREFIX = "PD^"
@@ -17,43 +19,70 @@ def index(phone, num, city):
     deviceids = r.smembers(PHONEDEVICE_PREFIX + phone)
     result = list()
     for deviceid in deviceids:
-        datas = r2.lrange(NEWHOUSELOG_PREFIX + deviceid.decode('utf-8'), 0, num)
+        datas = r.lrange(NEWHOUSELOG_PREFIX + deviceid.decode('utf-8'), 0, num)
         for data in datas:
             result.extend(json.loads(data.decode('utf-8')))
 
     cities, min_price, max_price, newhouses, newhouses_count = newhouse_handle(result, city)
-
     return render_template("house/house.html", newhouses=newhouses, userId=phone, num=num, cities=cities,
                            min_price=min_price, max_price=max_price, newhouses_count=newhouses_count)
 
 
+# def newhouse_handle(newhouse_json, city='南京'):
+#     try:
+#         newhouse_json = json.dumps(newhouse_json, ensure_ascii=False)
+#         df = pd.read_json(newhouse_json, orient='records')
+#         count = len(df)
+#         if count == 0:
+#             return list(), 0, 0, {}, count
+#         print(df.iloc[0])
+#         cities = df['CITY_NAME'].unique()
+#         df = df[df['CITY_NAME'] == city]
+#         print(df.loc[:, ['CONTEXT_ID', 'START_TIME']])
+#         print(df['PRICE_SHOW'].str.contains('元/㎡', na=False))
+#         df_price = df[df['PRICE_SHOW'].str.contains('元/㎡', na=False)].copy()
+#         min_price = min(df_price['PRICE_AVG'])
+#         max_price = max(df_price['PRICE_AVG'])
+#         df = df.sort_values(by='START_TIME', ascending=False)
+#         df_count = df.groupby('CONTEXT_ID').size().reset_index(name='COUNT')
+#         print(df_count.head(100))
+#         df_order = df.groupby('CONTEXT_ID').nth(0).copy()
+#         print(df_order.head(100))
+#         datas = df_order.merge(df_count, how='left', on='CONTEXT_ID')
+#         print(datas.head(100))
+#         datas.sort_values(by='COUNT', ascending=False, inplace=True)
+#         print(datas.head(100))
+#         datas = datas.to_json(orient="records", force_ascii=False)
+#         return cities, min_price, max_price, datas, count
+#     except Exception:
+#         print(traceback.format_exc())
+#         return list(), 0, 0, {}, count
+#
+#
 def newhouse_handle(newhouse_json, city='南京'):
-    newhouse_json = json.dumps(newhouse_json, ensure_ascii=False)
-    df = pd.read_json(newhouse_json, orient='records')
-    count = len(df)
-    cities = df['CITY_NAME'].unique()
-    df = df[df['CITY_NAME'] == city]
-    print(df.loc[:, ['CONTEXT_ID', 'START_TIME']])
-    min_price = min(df['PRICE_AVG'])
-    max_price = max(df['PRICE_AVG'])
-    df = df.sort_values(by='START_TIME', ascending=False)
-    df_count = df.groupby('CONTEXT_ID').size().reset_index(name='COUNT')
-    print(df_count.head(100))
-    df_order = df.groupby('CONTEXT_ID').nth(0).copy()
-    print(df_order.head(100))
-    datas = df_order.merge(df_count, how='left', on='CONTEXT_ID')
-    print(datas.head(100))
-    datas.sort_values(by='COUNT', ascending=False, inplace=True)
-    print(datas.head(100))
-    datas = datas.to_json(orient="records", force_ascii=False)
-    return cities, min_price, max_price, datas, count
+    try:
+        newhouse_json = json.dumps(newhouse_json, ensure_ascii=False)
+        df = pd.read_json(newhouse_json, orient='records')
+        count = len(df)
+        if count == 0:
+            return list(), 0, 0, {}, count
+        else:
+            object = newhouse.newhouse(df, city)
+            cities = object.get_cities()
+            min_price, max_price = object.get_price()
+            datas = object.get_result()
+            datas = datas.to_json(orient="records", force_ascii=False)
+        return cities, min_price, max_price, datas, count
+    except Exception:
+        print(traceback.format_exc())
+        return list(), 0, 0, {}, count
 
 
 if __name__ == '__main__':
     deviceids = r.smembers('PD^13851729904')
     result = list()
     for deviceid in deviceids:
-        datas = r2.lrange(NEWHOUSELOG_PREFIX + deviceid.decode('utf-8'), 0, 30)
+        datas = r.lrange(NEWHOUSELOG_PREFIX + deviceid.decode('utf-8'), 0, 30)
         for data in datas:
             result.extend(json.loads(data.decode('utf-8')))
 
---
+++ app/house/newhouse.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import json
+import traceback
+
+
+class newhouse:
+    def __init__(self, df, city='南京'):
+        self.df = df
+        self.city = city
+
+    def get_cities(self):
+        try:
+            cities = self.df['CITY_NAME'].unique()
+            return cities
+        except Exception:
+            print(traceback.format_exc())
+            return list()
+
+    def get_price(self):
+        try:
+            df_price = self.df[self.df['PRICE_SHOW'].str.contains('元/㎡', na=False)]
+            min_price = min(df_price['PRICE_AVG'])
+            max_price = max(df_price['PRICE_AVG'])
+            return min_price, max_price
+        except Exception:
+            print(traceback.format_exc())
+            return 0, 0
+
+    def get_result(self):
+        try:
+            df_result = self.df.sort_values(by='START_TIME', ascending=False)
+            df_count = df_result.groupby('CONTEXT_ID').size().reset_index(name='COUNT')
+            df_order = self.df.groupby('CONTEXT_ID').nth(0)
+            datas = df_order.merge(df_count, how='left', on='CONTEXT_ID')
+            datas.sort_values(by='COUNT', ascending=False, inplace=True)
+            return datas
+        except Exception:
+            print(traceback.format_exc())
+            return {}
