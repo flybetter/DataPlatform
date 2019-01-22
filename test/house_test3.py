91e10e8e3a505ea0dcd9353e1bee45f43880f16d
flybetter <flybetter@163.com>
2019-01-22 10:13:53 +0800

    find a bug but if you want the latest code to make sense you should restart the docker container.

--- test/house_test3.py
+++ test/house_test3.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     PD_PREFIX = 'PD^'
     NHLOG_PREFIX = 'NHLOG^'
     redis = Redis(host='202.102.83.162', db=1, port=6379)
-    results = redis.lrange(NHLOG_PREFIX+'2FFB77C7-6786-43C9-AEF9-1A8ABBAD90D8', 0, 30)
+    results = redis.lrange(NHLOG_PREFIX+'A5B9DA9F-40C6-4DE4-9539-3A288CE6B614', 0, 30)
     data = list()
     for result in results:
         data.extend(json.loads(result.decode('utf-8')))
