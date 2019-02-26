2ca00e0c529af4a170027d7a8a430ce476cadedd
flybetter <flybetter@163.com>
2019-02-26 13:26:18 +0800

    fix one error bug

--- app/house/secondhouseDetail.py
+++ app/house/secondhouseDetail.py
@@ -97,56 +97,71 @@ class secondhouseDetail:
         return count_df.to_json(orient='values', index=True)
 
     def get_avg_price_histogram(self):
-        self.fiter_df['avg_price_range'] = pd.cut(self.fiter_df['PRICE_AVG'], bins=20, right=False)
+        self.fiter_df['avg_price_range'] = pd.cut(self.fiter_df['PRICE_AVG'], bins=20, right=False, duplicates='drop')
         value_df = self.fiter_df.groupby("avg_price_range").size().reset_index(name='counts')
         return (value_df['avg_price_range'].astype('str').to_json(orient='values', index=True),
                 value_df['counts'].to_json(orient='values', index=True))
 
     def get_area_histogram(self):
-        self.df['PIC_AREA'] = pd.cut(self.df['PIC_AREA'], bins=10, right=False)
-        value_df = self.df.groupby("PIC_AREA").size().reset_index(name='counts')
-        return (value_df['PIC_AREA'].astype('str').to_json(orient='values', index=True),
-                value_df['counts'].to_json(orient='values', index=True))
+        try:
+            self.df['PIC_AREA'] = pd.cut(self.df['PIC_AREA'], bins=10, right=False, duplicates='drop')
+            value_df = self.df.groupby("PIC_AREA").size().reset_index(name='counts')
+            return (value_df['PIC_AREA'].astype('str').to_json(orient='values', index=True),
+                    value_df['counts'].to_json(orient='values', index=True))
+        except Exception:
+            return ('null', 'null')
 
     def get_sum_price_histogram(self):
-
-        self.df['PIC_HX_TOTALPRICE'] = pd.cut(pd.to_numeric(
-            self.df['PIC_HX_TOTALPRICE'].astype(str).map(lambda x: re.sub(u'[\u4E00-\u9FA5]', '', x)),
-            errors='coerce'),
-            bins=20, right=False)
-
-        value_df = self.df.groupby("PIC_HX_TOTALPRICE").size().reset_index(name='counts')
-
-        return (value_df['PIC_HX_TOTALPRICE'].astype('str').to_json(orient='values', index=True),
-                value_df['counts'].to_json(orient='values', index=True))
+        try:
+            self.df['PIC_HX_TOTALPRICE'] = pd.cut(pd.to_numeric(
+                self.df['PIC_HX_TOTALPRICE'].astype(str).map(lambda x: re.sub(u'[\u4E00-\u9FA5]', '', x)),
+                errors='coerce'), bins=20, right=False, duplicates='drop')
+            value_df = self.df.groupby("PIC_HX_TOTALPRICE").size().reset_index(name='counts')
+            return (value_df['PIC_HX_TOTALPRICE'].astype('str').to_json(orient='values', index=True),
+                    value_df['counts'].to_json(orient='values', index=True))
+        except Exception:
+            return ('null', 'null')
 
     def get_toilet_pie(self):
-        temp = pd.DataFrame(
-            {'Percentage': self.df.groupby("PIC_WEI").size() / len(self.df['PIC_WEI'].dropna())}).reset_index().rename(
-            columns={'PIC_WEI': 'name', 'Percentage': 'y'})
-        temp['name'] = temp['name'].astype(str).map(lambda x: x.replace('.0', '间'))
-        return temp.to_json(orient='records', index=True)
+        try:
+            temp = pd.DataFrame(
+                {'Percentage': self.df.groupby("PIC_WEI").size() / len(
+                    self.df['PIC_WEI'].dropna())}).reset_index().rename(
+                columns={'PIC_WEI': 'name', 'Percentage': 'y'})
+            temp['name'] = temp['name'].astype(str).map(lambda x: x.replace('.0', '间'))
+            return temp.to_json(orient='records', index=True)
+        except Exception:
+            return 'null'
 
     def get_bedroom_pie(self):
-        di = {8: 1, 9: 2, 10: 3, 11: 4, 21: 5, 22: 6}
-        df = self.df.replace({"PIC_TYPE": di})
-        temp = pd.DataFrame(
-            {'Percentage': df.groupby("PIC_TYPE").size() / len(df['PIC_TYPE'].dropna())}).reset_index().rename(
-            columns={'PIC_TYPE': 'name', 'Percentage': 'y'})
-        temp['name'] = temp['name'].astype(str).map(lambda x: x.replace('.0', '室'))
-        return temp.to_json(orient='records', index=True)
+        try:
+            di = {8: 1, 9: 2, 10: 3, 11: 4, 21: 5, 22: 6}
+            df = self.df.replace({"PIC_TYPE": di})
+            temp = pd.DataFrame(
+                {'Percentage': df.groupby("PIC_TYPE").size() / len(df['PIC_TYPE'].dropna())}).reset_index().rename(
+                columns={'PIC_TYPE': 'name', 'Percentage': 'y'})
+            temp['name'] = temp['name'].astype(str).map(lambda x: x.replace('.0', '室'))
+            return temp.to_json(orient='records', index=True)
+        except Exception:
+            return 'null'
 
     def get_livingroom_pie(self):
-        temp = pd.DataFrame(
-            {'Percentage': self.df.groupby("PIC_TING").size() / len(
-                self.df['PIC_TING'].dropna())}).reset_index().rename(
-            columns={'PIC_TING': 'name', 'Percentage': 'y'})
-        temp['name'] = temp['name'].astype(str).map(lambda x: x.replace('.0', '间'))
-        return temp.to_json(orient='records', index=True)
+        try:
+            temp = pd.DataFrame(
+                {'Percentage': self.df.groupby("PIC_TING").size() / len(
+                    self.df['PIC_TING'].dropna())}).reset_index().rename(
+                columns={'PIC_TING': 'name', 'Percentage': 'y'})
+            temp['name'] = temp['name'].astype(str).map(lambda x: x.replace('.0', '间'))
+            return temp.to_json(orient='records', index=True)
+        except Exception:
+            return 'null'
 
     def get_kitchen_pie(self):
-        temp = pd.DataFrame(
-            {'Percentage': self.df.groupby("PIC_CHU").size() / len(self.df['PIC_CHU'].dropna())}).reset_index().rename(
-            columns={'PIC_CHU': 'name', 'Percentage': 'y'})
-        temp['name'] = temp['name'].astype(str).map(lambda x: x.replace('.0', '间'))
-        return temp.to_json(orient='records', index=True)
+        try:
+            temp = pd.DataFrame(
+                {'Percentage': self.df.groupby("PIC_CHU").size() / len(self.df['PIC_CHU'].dropna())}).reset_index().rename(
+                columns={'PIC_CHU': 'name', 'Percentage': 'y'})
+            temp['name'] = temp['name'].astype(str).map(lambda x: x.replace('.0', '间'))
+            return temp.to_json(orient='records', index=True)
+        except Exception:
+            return 'null'
